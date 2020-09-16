3f1eff42fa298fd34696fee884a0d886c750ec05
SDMadhumitha <madhumithakd26@gmail.com>
2020-09-15 22:12:02 -0700

    added

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,28 @@
+package org.edu;
+
+public class Arts extends Education {
+private void bSc() {
+System.out.println("Bachelor of Science");
+}
+private void bEd() {
+System.out.println("Bachelor of Education");
+}
+private void bA() {
+System.out.println("Bachelor of Arts");
+}
+private void bBA() {
+System.out.println("Bachelor of Business Administration");
+}
+@Override
+	public void ug(String ug) {
+		System.out.println("UG is "+ug);
+	}
+public static void main(String[] args) {
+	Arts a = new Arts();
+	a.bSc();
+	a.bEd();
+	a.bA();
+	a.bBA();
+	a.ug("BCA");
+}
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,10 @@
+package org.edu;
+
+public class Education {
+	public void ug(String ug) {
+System.out.println(ug);
+}
+	public void pg(String pg) {
+System.out.println(pg);
+}
+}
